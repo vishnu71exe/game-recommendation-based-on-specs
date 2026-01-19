e81ea6468206af984e6ed550de2a410c6e532483
vishnu71exe <vishnuexe71@gmail.com>
2026-01-19 22:47:29 +0530

    new files

--- db_contentpull.py
+++ db_contentpull.py
@@ -3,7 +3,7 @@ import sqlite3
 connection=sqlite3.connect("data6.db")
 cur=connection.cursor()
 
-cur.execute("SELECT * FROM game_details where appid=22380")
+cur.execute("SELECT * FROM game_details where appid=1091500")
 #cur.execute("SELECT * FROM game_details ")
 #cur.execute("SELECT COUNT(appid) from game_details")
 results=cur.fetchall()
